29469df2bdab7f43772c929f301bc8f76cdf9df0
Philipp <46283819+PhilSwiss@users.noreply.github.com>
2025-10-08 08:45:20 +0200

    option for mem-address added

--- decoders/decoder-raw.js
+++ decoders/decoder-raw.js
@@ -1,30 +1,32 @@
-// title:  Raw Decoder v4
+// title:  Raw Decoder v5
 // author: Decca/Rift
-// desc:   just decode to memory
+// desc:   just decode to memory, incl. adr-param, 3 digits for width
 // script: js
 
 // DESCRIPTION
 //
 // store tiles/sprites (data) as string
 //  format: <offset><width><colorvalues>
-//  digits: <5 digits><2 digits><rest of digits>
+//  digits: <5 digits><3 digits><rest of digits>
 //  offset: tiles = 08000 / 08100 / 08200 / 08300
 //          sprites = 0c000 / 0c100 / 0c200 / 0c300
 //          palette = 07F80 / systemfont = 28E08
-//   width: 08 (64) up to 80 (1024)
-var gfx = "0800008caaaaaccccfccaacccdcc25cccacc4fcccfccaaccaaaaaccccaccccc0aaa0ccc"
+//   width: 040 (64) up to 400 (1024)
+var gfx = "08000040caaaaaccccfccaacccdcc25cccacc4fcccfccaaccaaaaaccccaccccc0aaa0ccc"
+
+// the offset from the data-string can be overwritten by providing
+// an optional address when calling the function: tomem(gfx,0x8400)
 
 // CODEBLOCK
 
-// the raw decoder
-function tomem(str) {
-  var psi=parseInt
-  var o=psi(str.substring(0,5),16) // get (o)ffset
-  var w=psi(str.substring(5,7),16)*8-1 // get (w)idth
-  var d=str.substring(7,str.length) // remove header to get (d)ata
+// the raw-decoder
+function tomem(str,adr) {
+  var o=adr || parseInt(str.substring(0,5),16) // get (o)ffset, from param or string
+  var w=parseInt(str.substring(5,8),16)-1 // get (w)idth
+  var d=str.substring(8,str.length) // remove header to get (d)ata
   var y=0
   for (var x = 0;x <= d.length;x++) { // write to mem
-    var c=psi(d.substring(x,x+1),16) // get (c)olor value
+    var c=parseInt(d.substring(x,x+1),16) // get (c)olor value
     poke4(o+y,c); y=y+1
     if (y>w) {y=0; o=o+1024}
   }
--- decoders/decoder-raw.lua
+++ decoders/decoder-raw.lua
@@ -1,30 +1,32 @@
--- title:  Raw Decoder v4
+-- title:  Raw Decoder v5
 -- author: Decca/Rift
--- desc:   just decode to memory
+-- desc:   just decode to memory, incl. adr-param, 3 digits for width
 -- script: lua
 
 -- DESCRIPTION
 --
 -- store tiles/sprites (data) as string
 --  format: <offset><width><colorvalues>
---  digits: <5 digits><2 digits><rest of digits>
+--  digits: <5 digits><3 digits><rest of digits>
 --  offset: tiles = 08000 / 08100 / 08200 / 08300
 --          sprites = 0c000 / 0c100 / 0c200 / 0c300
 --          palette = 07F80 / systemfont = 28E08
---   width: 08 (64) up to 80 (1024)
-local gfx = "0800008caaaaaccccfccaacccdcc25cccacc4fcccfccaaccaaaaaccccaccccc0aaa0ccc"
+--   width: 040 (64) up to 400 (1024)
+local gfx = "08000040caaaaaccccfccaacccdcc25cccacc4fcccfccaaccaaaaaccccaccccc0aaa0ccc"
+
+-- the offset from the data-string can be overwritten by providing
+-- an optional address when calling the function: tomem(gfx,0x8400)
 
 -- CODEBLOCK
 
--- the raw decoder
-function tomem(str)
-  local tnb=tonumber
-  local o=tnb(str:sub(1,5),16) -- get (o)ffset
-  local w=tnb(str:sub(6,7),16)*8-1 -- get (w)idth
-  local d=str:sub(8,str:len()) -- remove header to get (d)ata
+-- the raw-decoder
+function tomem(str,adr)
+  local o=adr or tonumber(str:sub(1,5),16) -- get (o)ffset, from param or string
+  local w=tonumber(str:sub(6,8),16)-1 -- get (w)idth
+  local d=str:sub(9,str:len()) -- remove header to get (d)ata
   local y=0
   for x = 1,#d,1 do -- write to mem
-    local c=tnb(d:sub(x,x),16) -- get (c)olor value
+    local c=tonumber(d:sub(x,x),16) -- get (c)olor value
     poke4(o+y,c) y=y+1
     if y>w then y=0 o=o+1024 end
   end
